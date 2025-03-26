7ef248c3f200eedd4b222ffc171f315e791185a3
wilmer_sanchez <wilmerjaviers@yahoo.com>
2025-03-25 19:05:30 -0600

    buscador.jsx listo

--- ProyectoG3_contactos/src/index.js
+++ ProyectoG3_contactos/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import './index.css';  // Importar después de Bootstrap para sobrescribir sus estilos
+import './index.css'; 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
